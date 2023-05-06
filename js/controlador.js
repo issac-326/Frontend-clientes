24cfa8579c8a1769461410ab0dd1c822f4e08938
issc <issacjamaelc00@gmail.com>
2023-05-06 11:50:57 -0600

    botoncarrito

--- js/controlador.js
+++ js/controlador.js
@@ -1,3 +1,5 @@
+var pedido = [];
+
 function generarCategorias(){
 
     fetch('/categorias', {
@@ -121,7 +123,8 @@ function opcionesMenuIndividuales(tituloRestaurante){
                     <div><p style="font-size: 16px;">${opcionMenu.descripcion}</p></div>
                     <div style="display: flex; justify-content: space-between;">
                         <div><p style="font-size: 20px;">LPS. ${opcionMenu.precio}</p></div>
-                        <div><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#ventana" id="botonOrdenar" onclick="modalCantidadOrdenar('${opcionMenu.precio}')">Ordenar</button></div>
+                        <div><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#ventana" 
+                        id="botonOrdenar" onclick="modalCantidadOrdenar('${opcionMenu.precio}', '${opcionMenu.nomComida}', '${opcionMenu.imagen}', '${tituloRestaurante}')">Ordenar</button></div>
                     </div>
                 </div>
                 <div style="border-right: 0;">
@@ -154,7 +157,7 @@ function opcionesMenuFamiliares(tituloRestaurante){
                     <div><p style="font-size: 16px;">${opcionMenu.descripcion}</p></div>
                     <div style="display: flex; justify-content: space-between;">
                         <div><p style="font-size: 20px;">LPS. ${opcionMenu.precio}</p></div>
-                        <div><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#ventana" id="botonOrdenar" onclick="modalCantidadOrdenar('${opcionMenu.precio}')">Ordenar</button></div>
+                        <div><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#ventana" id="botonOrdenar" onclick="modalCantidadOrdenar('${opcionMenu.precio}', '${opcionMenu.nomComida}', '${opcionMenu.imagen}', '${tituloRestaurante}')">Ordenar</button></div>
                     </div>
                 </div>
                 <div style="border-right: 0;">
@@ -187,7 +190,7 @@ function opcionesMenuComplementos(tituloRestaurante){
                     <div><p style="font-size: 16px;">${opcionMenu.descripcion}</p></div>
                     <div style="display: flex; justify-content: space-between;">
                         <div><p style="font-size: 20px;">LPS. ${opcionMenu.precio}</p></div>
-                        <div><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#ventana" id="botonOrdenar" onclick="modalCantidadOrdenar('${opcionMenu.precio}')">Ordenar</button></div>
+                        <div><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#ventana" id="botonOrdenar" onclick="modalCantidadOrdenar('${opcionMenu.precio}', '${opcionMenu.nomComida}', '${opcionMenu.imagen}', '${tituloRestaurante}')">Ordenar</button></div>
                     </div>
                 </div>
                 <div style="border-right: 0;">
@@ -200,7 +203,7 @@ function opcionesMenuComplementos(tituloRestaurante){
         }); 
 }
 
-function modalCantidadOrdenar(precio){
+function modalCantidadOrdenar(precio, nomComida, imagenC, tituloRestaurante){
     document.getElementById('modalOrdenar').innerHTML +=
     `
     <div class="modal fade" id="ventana" tabindex="-1" role="dialog" aria-hidden="true">
@@ -220,7 +223,7 @@ function modalCantidadOrdenar(precio){
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-                    <button type="button" class="btn btn-primary" onclick="agregarAlPedido(${parseInt(precio)})" style="background-color: #4F3EA3" id="ordenar">Ordenar</button>
+                    <button type="button" class="btn btn-primary" onclick="agregarAlPedido(${parseInt(precio)}, '${nomComida}', '${imagenC}', '${tituloRestaurante}')" style="background-color: #4F3EA3" id="ordenar">Ordenar</button>
                 </div>
             </div>
         </div>
@@ -229,8 +232,21 @@ function modalCantidadOrdenar(precio){
     abirModal();
 }
 
-function agregarAlPedido(precio){
-    console.log(precio)
+function agregarAlPedido(precioC, nomComida, imagenC, tituloRestaurante){
+    var cantidadInput = document.getElementById("cantidad").value;
+    var cantidadActual = parseInt(cantidadInput);
+    let restau = tituloRestaurante;
+    let plato = nomComida;
+    let imagenPublicitaria = imagenC
+    let encargo ={
+        restaurante: restau,
+        nombre: plato,
+        imagen: imagenPublicitaria,
+        precio: precioC,
+        cantidad: cantidadActual
+    }
+    pedido.push(encargo);
+    console.log(pedido);
     $('#ventana').modal('hide');
 }
 
@@ -255,7 +271,70 @@ function sumar(){
     
 
 }
-  
+
+function mostrarPedido(){
+    let factura = 0;
+    let impuesto;
+    document.getElementById("pedidos").innerHTML = " ";
+    for(let i=0; i<pedido.length; i++){
+        factura += (pedido[i].precio * pedido[i].cantidad);
+        document.getElementById("pedidos").innerHTML += `
+        <div class="contenedor-carrito">
+            <div style="margin-left: 10px;">
+                <img style="height: 150px;" src="${pedido[i].imagen}">
+            </div>
+            <div id="flex-infCarrito">
+                    <div class="productos-carrito">${pedido[i].restaurante}:</div>
+                    <div class="productos-carrito">${pedido[i].nombre}</div>
+                    <div class="productos-carrito">precio: Lps ${pedido[i].precio}</div>
+                    <div class="productos-carrito">cantidad: ${pedido[i].cantidad}</div>
+            </div>
+        </div>
+        `
+    }
+    impuesto = factura*0.15;
+    if(pedido != ''){
+    document.getElementById("pedidos").innerHTML += `
+    <div style="display: flex; margin-left: 30px; justify-content: space-between; margin-right: 30px;">
+        <p>
+            impuestos:
+        </p>
+        <p>
+            ${impuesto}
+        </p>
+    </div>
+    <div style="display: flex; margin-left: 30px; justify-content: space-between; margin-right: 30px;">
+        <p>total:</p>
+        <p>${factura}</p>
+    </div>
+    `
+    }
+}
+
+function enviarPedido(){
+    let usuario = localStorage.getItem('usuario');
+    let direccion = document.getElementById("direccion").value;
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+    "id": usuario,
+    "listaProductos": pedido,
+    "direccion": direccion  
+    });
+
+    var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+    };
+
+    fetch("/pedidos/nuevo", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+}
 
 function abirModal(){
     $('#ventana').modal('show');
